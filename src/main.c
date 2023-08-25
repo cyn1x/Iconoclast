f11aa47ac5c81917918b8e2c6ba364338d1845fb
Zac Vukovic <zac@cyn1x.com>
2023-08-25 13:51:23 +1000

    Implemented the program window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,73 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    MessageBox(0, "Rakija Engine initialised", "Rakija Engine", MB_OK|MB_ICONINFORMATION);
+    const wchar_t CLASS_NAME[]  = L"Rakija Engine Window Class";
+    
+    WNDCLASS wc = {0};
+    
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    
+    RegisterClass(&wc);
+    
+    HWND hwnd = CreateWindowEx(
+                               0,
+                               CLASS_NAME,
+                               L"Rakija Engine",
+                               WS_OVERLAPPEDWINDOW,
+                               
+                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               
+                               NULL,
+                               NULL,
+                               hInstance,
+                               NULL
+                               );
+    
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+    
+    ShowWindow(hwnd, nCmdShow);
+    
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+        
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+            
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+        
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
