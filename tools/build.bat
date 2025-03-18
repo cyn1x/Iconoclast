42ba2eefd66db76e490655a2f1ce28a6e8ba451b
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2025-03-18 21:13:40 +1100

    Updated build.bat

--- tools/build.bat
+++ tools/build.bat
@@ -71,11 +71,11 @@ goto :build
 :mkdirs
 rem Make required directories for engine compilation
 if not exist %1\obj mkdir %1\obj
-if not exist %1\obj\%config%_x64 mkdir %1\obj\%config%_x64
-if not exist %1\obj\%config%_x86 mkdir %1\obj\%config%_x86
+if not exist %1\obj\Windows\%config%_x64 mkdir %1\obj\Windows\%config%_x64
+if not exist %1\obj\Windows\%config%_x86 mkdir %1\obj\Windows\%config%_x86
 if not exist %1\bin mkdir %1\bin
-if not exist %1\bin\%config%_x64 mkdir %1\bin\%config%_x64
-if not exist %1\bin\%config%_x86 mkdir %1\bin\%config%_x86
+if not exist %1\bin\Windows\%config%_x64 mkdir %1\bin\Windows\%config%_x64
+if not exist %1\bin\Windows\%config%_x86 mkdir %1\bin\Windows\%config%_x86
 
 goto :eof
 
@@ -88,8 +88,11 @@ call :include
 
 rem Change directory to intermediate dir
 pushd obj
+pushd Windows
 
 if %target%==x64 ( pushd %config%_x64 ) else ( pushd %config%_x86 )
+
+echo %cd%
 
 call :sources
 goto :compile
@@ -104,7 +107,7 @@ for /r include %%F in (*.h) do (
 
     rem Prevent duplicate include directories
     if !prev! neq !relpath! (
-        call set "incs=%%incs%% -I..\..\..!relpath:~0,-1!"
+        call set "incs=%%incs%% -I..\..\..\..!relpath:~0,-1!"
 
         rem Invert slashes for include paths to be compatible with the LSP
         set cpath=!relpath:\=/!
@@ -120,72 +123,77 @@ goto :eof
 
 rem Recursively set all relative file paths of *.cpp source files
 :sources
-for /r ..\..\src %%F in (*.cpp) do (
+for /r ..\..\..\src %%F in (*.cpp) do (
     set "abspath=%%F"
     set "relpath=!abspath:%cmp%=!"
-    call set "srcs=%%srcs%% ..\..\..!relpath!"
+    call set "srcs=%%srcs%% ..\..\..\..!relpath!"
 )
 
 rem End of :sources subroutine call
 goto :eof
 
 :compile
-cl /EHsc /c /LD -MD -Z7 -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -FAsc /std:c++20 /Fo"..\..\obj\\%config%_%platform%\\" %incs% %compilerFlags% /Fa"..\..\obj\\%config%_%target%\\" %srcs:~1%
+cl /EHsc /c /LD -MD -Z7 -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -FAsc /std:c++20 /Fo"..\..\..\obj\\Windows\\%config%_%platform%\\" %incs% %compilerFlags% /Fa"..\..\..\obj\\Windows\\%config%_%target%\\" %srcs:~1%
 
-rem Pop to bin dir and pop to project dir
+rem Pop to platform dir, bin dir, and project dir
+popd
 popd
 popd
 
 pushd bin
+pushd Windows
 
 if %target%==x64 ( pushd %config%_x64 ) else ( pushd %config%_x86 )
 
 rem Store all *.obj file names only for the linker
-for /r ..\..\obj %%F in (%config%_%platform%\*.obj) do (
-    call set "objs=%%objs%% obj\%config%_%target%\%%~nxF"
+for /r ..\..\..\obj %%F in (%config%_%platform%\*.obj) do (
+    call set "objs=%%objs%% obj\Windows\%config%_%target%\%%~nxF"
 )
 
-rem Pop to bin dir and pop to root dir
+rem Pop to platform dir, bin dir, and root dir
+popd
 popd
 popd
 
-LINK /nologo %linkerFlags% %objs:~1% /MAP:bin\%config%_%target%\iconoclast_%target%.map /DLL /OUT:bin\%config%_%target%\iconoclast_%target%.dll user32.lib gdi32.lib
+LINK /nologo %linkerFlags% %objs:~1% /MAP:bin\Windows\%config%_%target%\iconoclast_%target%.map /DLL /OUT:bin\Windows\%config%_%target%\iconoclast_%target%.dll user32.lib gdi32.lib
 
 popd
 
 rem Copy DLL to where the test executable will be built
-copy Iconoclast\bin\%config%_%target%\%dll% Sandbox\bin\%config%_%target%\%dll% > nul
+copy Iconoclast\bin\Windows\%config%_%target%\%dll% Sandbox\bin\Windows\%config%_%target%\%dll% > nul
 
 rem Change directory to Sandbox project dir and intermediate dir
 pushd Sandbox
 pushd obj
+pushd Windows
 
 if %target%==x64 ( pushd %config%_x64 ) else ( pushd %config%_x86 )
 
 set cmp=!root:~0!Sandbox
 
 rem Store all Sandbox *.cpp source files in a variable
-for /r ..\..\src %%F in (*.cpp) do (
+for /r ..\..\..\src %%F in (*.cpp) do (
     set "abspath=%%F"
     set "relpath=!abspath:%cmp%=!"
-    call set "exesrcs=%%exesrcs%% ..\..!relpath!"
+    call set "exesrcs=%%exesrcs%% ..\..\..!relpath!"
 )
 
 rem Compile *.cpp files
-cl /nologo /EHsc /c /MD /Zi /W4 /Wall /std:c++20 /Fo"..\..\obj\\%config%_%platform%\\" /Fd"..\..\obj\\%config%_%platform%\\" %exesrcs% %incs%
+cl /nologo /EHsc /c /MD /Zi /W4 /Wall /std:c++20 /Fo"..\..\..\obj\\Windows\\%config%_%platform%\\" /Fd"..\..\..\obj\\Windows\\%config%_%platform%\\" %exesrcs% %incs%
 
 rem Compile Sandbox program and link DLL
 
 rem Pop to obj dir and then project dir
 popd
 popd
+popd
 
 rem Store all *.obj file names only for the linker
 for /r obj %%F in (*.obj) do (
-    call set "exeobjs=%%exeobjs%% obj\%config%_%target%\%%~nxF"
+    call set "exeobjs=%%exeobjs%% obj\Windows\%config%_%target%\%%~nxF"
 )
 
 rem Link *.obj object files
-LINK /DEBUG %exeobjs:~1% /OUT:bin\%config%_%target%\%exe% ..\Iconoclast\bin\%config%_%target%\iconoclast_%target%.lib
+LINK /DEBUG %exeobjs:~1% /OUT:bin\Windows\%config%_%target%\%exe% ..\Iconoclast\bin\Windows\%config%_%target%\iconoclast_%target%.lib
 
 rem Build completed
